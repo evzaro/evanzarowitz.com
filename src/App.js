fad5a414e20c47622860a9c2094476e1eed8126d
evzaro <evan@props.us>
2017-04-19 13:38:59 -0400

    gh pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import './App.css';
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+
+import Helmet from 'react-helmet';
 
 class App extends Component {
   render() {
