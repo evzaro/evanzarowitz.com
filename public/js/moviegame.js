42791cec884123855e0e8a5641b6922c2b68124d
Evan Zarowitz <evan.zarowitz@nytimes.com>
2025-01-03 14:06:48 -0500

    game updates

--- public/js/moviegame.js
+++ public/js/moviegame.js
@@ -5,7 +5,16 @@ let quizDone = false;
 const phraseDisplay = document.getElementById("phrase-display");
 const feedback = document.getElementById("feedback");
 
+const localStorageKey = 'hasSeenModal';
+
+// starting logic
 document.addEventListener("DOMContentLoaded", function () {
+  const hasSeenModal = localStorage.getItem(localStorageKey);
+  if (!hasSeenModal) {
+    openModal();
+    localStorage.setItem(localStorageKey, true);
+  }
+
   const today = new Date();
   const dayNumber = Math.floor(today / (1000 * 60 * 60 * 24));
   const index = dayNumber % data.length;
@@ -86,55 +95,105 @@ function revealPortion() {
   } else if (currentGuess === 2) {
     let phraseArray = document
       .getElementById("phrase-display")
-      .textContent.split("");
-    phraseArray[1] = todayQuiz.answer[1];
-    phraseArray[phraseArray.length - 2] =
-      todayQuiz.answer[todayQuiz.answer.length - 2];
+      .textContent.split(" ");
+    let answerArray = todayQuiz.answer.split(" ");
+    phraseArray[0] = answerArray[0];
     document.getElementById("phrase-display").textContent =
-      phraseArray.join("");
+      phraseArray.join(" ");
   } else if (currentGuess === 3) {
     let phraseArray = document
       .getElementById("phrase-display")
-      .textContent.split("");
-    phraseArray[2] = todayQuiz.answer[2];
+      .textContent.split(" ");
+    let answerArray = todayQuiz.answer.split(" ");
+    phraseArray[phraseArray.length - 1] = answerArray[answerArray.length - 1];
     document.getElementById("phrase-display").textContent =
-      phraseArray.join("");
+      phraseArray.join(" ");
   } else if (currentGuess === maxGuesses) {
     document.getElementById("phrase-display").textContent = todayQuiz.answer;
   }
 }
 
+// instructions modal
+function openModal() {
+  const modal = document.getElementById("how-to-play-modal");
+  modal.style.display = "block";
+}
+
+// Function to close the modal
+function closeModal() {
+  const modal = document.getElementById("how-to-play-modal");
+  modal.style.display = "none";
+}
+
+// Event listener for opening the modal
+document.getElementById("openModalBtn").addEventListener("click", openModal);
+
+// Event listener for closing the modal
+document.getElementById("closeModalBtn").addEventListener("click", closeModal);
+
+// Also close the modal if the user clicks outside of the modal content
+window.addEventListener("click", (event) => {
+  const modal = document.getElementById("how-to-play-modal");
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
+
 const data = [
   {
-    plot: "Venturing through limitless realities, a brilliant mind traverses extraordinary dimensions, seeking to unravel the fundamental truths of existence amidst chaos.",
-    answer: "The Theory of Everything Everywhere All at Once",
+    plot: `Venturing through limitless realities, a brilliant mind traverses extraordinary dimensions, seeking to unravel the fundamental truths of existence amidst chaos.`,
+    answer: `The Theory of Everything Everywhere All at Once`,
   },
   {
-    plot: "As the line between perception and reality blurs, a woman’s mysterious disappearance intertwines with self-discovery and the battle against inner turmoil.",
-    answer: "Gone Girl Interrupted",
+    plot: `As the line between perception and reality blurs, a woman’s mysterious disappearance intertwines with self-discovery and the battle against inner turmoil.`,
+    answer: `Gone Girl Interrupted`,
   },
   {
-    plot: "A figure of immense strength finds unexpected alliance and support amidst a community bound by resilience and compassion, discovering the true essence of invulnerability and human connection.",
-    answer: "Man of Steel Magnolias",
+    plot: `A figure of immense strength finds unexpected alliance and support amidst a community bound by resilience and compassion, discovering the true essence of invulnerability and human connection.`,
+    answer: `Man of Steel Magnolias`,
   },
   {
-    plot: "In a tale of transformation and redemption, a steadfast creature and a misunderstood figure forge a deep bond through shared adventures, revealing the true essence of resilience in the face of adversity.",
-    answer: "Black Beauty and the Beast",
+    plot: `In a tale of transformation and redemption, a steadfast creature and a misunderstood figure forge a deep bond through shared adventures, revealing the true essence of resilience in the face of adversity.`,
+    answer: `Black Beauty and the Beast`,
   },
   {
-    plot: "Amidst a desolate and treacherous landscape, a relentless pursuit drives individuals towards redemption and retribution, balancing chaos with a search for personal salvation.",
-    answer: "Mad Max Fury Road to Perdition",
+    plot: `Amidst a desolate and treacherous landscape, a relentless pursuit drives individuals towards redemption and retribution, balancing chaos with a search for personal salvation.`,
+    answer: `Mad Max Fury Road to Perdition`,
   },
   {
-    plot: "As society faces extinction, a guardian partnered with a secretive organization must fend off extraordinary cosmic threats to secure humanity’s future.",
-    answer: "Children of Men in Black",
+    plot: `As society faces extinction, a guardian partnered with a secretive organization must fend off extraordinary cosmic threats to secure humanity’s future.`,
+    answer: `Children of Men in Black`,
   },
   {
-    plot: "Set against landscapes rich with opportunity and danger, individuals vie for precious resources while confronting the moral costs of their relentless ambition.",
-    answer: "There Will Be Blood Diamond",
+    plot: `Set against landscapes rich with opportunity and danger, individuals vie for precious resources while confronting the moral costs of their relentless ambition.`,
+    answer: `There Will Be Blood Diamond`,
   },
   {
-    plot: "In a quest across vast celestial bodies, explorers face a world transformed by intelligent beings, uncovering secrets that challenge the very notion of humanity’s place in the universe.",
-    answer: "Treasure Planet of the Apes",
+    plot: `In a quest across vast celestial bodies, explorers face a world transformed by intelligent beings, uncovering secrets that challenge the very notion of humanity’s place in the universe.`,
+    answer: `Treasure Planet of the Apes`,
+  },
+  {
+    plot: `In a small town setting, a frustrated man undergoes a profound change when he unexpectedly bonds with an enigmatic individual who reveals a world beyond appearances and challenges him to see life differently.`,
+    answer: `American Beauty and the Beast`,
+  },
+  {
+    plot: `In a chaotic blend of tradition and rebellion, a group of misfits navigates the complexities of honor and loyalty, challenging societal norms with their unorthodox ways.`,
+    answer: `Animal House of Flying Daggers`,
+  },
+  {
+    plot: `In a tense exploration of identity and justice, a young individual attempting to navigate life while concealing their true identity crosses paths with a pair of rebellious detectives determined to uphold the law, leading to a journey of self-discovery and unexpected alliances.`,
+    answer: `Bad Boys Don't Cry`,
+  },
+  {
+    plot: `When chaos erupts in a city as museum exhibits mysteriously come to life each night, a vigilante hero and a bumbling night guard must team up to restore order and prevent an iconic villain from seizing control of history's greatest artifacts.`,
+    answer: `The Dark Knight Night at the Museum`,
+  },
+  {
+    plot: `As a zombie apocalypse unfurls, a group of charismatic teachers inspires their students to seize the day and break societal norms, all while battling the undead with unorthodox defensive methods and unconventional wisdom.`,
+    answer: `Shaun of the Dead Poets Society,`,
+  },
+  {
+    plot: `No Time to Die Hard`,
+    answer: `A seasoned secret agent finds himself trapped in a skyscraper under siege, where he must team up with a resourceful NYPD officer to thwart a global terrorist threat and protect valuable secrets from falling into the wrong hands.`,
   },
 ];
